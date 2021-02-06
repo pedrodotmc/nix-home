9fbcf6817732087b888f6ed2eb7a0eef39c88e3c
Pedro Miranda <pedrodotmc@gmail.com>
2021-02-05 20:43:29 -0500

    Update nix-home repository
    Signed-off-by: Pedro Miranda <pedrodotmc@gmail.com>

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 # Copy home.nix file
 mkdir -p ~/.config/nixpkgs
-cp user/config.nix ~/.config/nixpkgs
-cp user/home.nix ~/.config/nixpkgs
+cp -l -f user/config.nix ~/.config/nixpkgs
+cp -l -f user/home.nix ~/.config/nixpkgs
 
 # Run home installation
 home-manager switch
 
 # Install VSCode extensions
-xargs code --install-extension < scripts/vscode_extensions
+xargs code --install-extension < files/vscode_extensions
